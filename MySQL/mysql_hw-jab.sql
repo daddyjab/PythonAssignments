0228c6434de9e859b7db64be0cbf321bf8622738
Jeff Brown <39058795+daddyjab@users.noreply.github.com>
2019-01-25 01:13:21 -0600

    finished up to 5a

--- MySQL/mysql_hw-jab.sql
+++ MySQL/mysql_hw-jab.sql
@@ -29,16 +29,50 @@ Select country_id, country from country
 where country in ('Afghanistan', 'Bangladesh', 'China');
 
 -- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a column in the table actor named description and use the data type BLOB (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
+alter table actor
+add column description blob;
 
+select * from actor;
 
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
+alter table actor
+drop column description;
+
+select * from actor;
 
 -- 4a. List the last names of actors, as well as how many actors have that last name.
+select last_name, count(last_name) from actor
+group by last_name
+order by last_name asc;
+
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+select last_name, count(last_name) from actor
+group by last_name
+having count(last_name)>=2
+order by count(last_name) desc;
+
 -- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
+select * from actor
+where first_name='GROUCHO' or first_name='HARPO';
+
+update actor
+set first_name = 'HARPO'
+where first_name = 'GROUCHO' and last_name = 'WILLIAMS';
+
+select * from actor
+where first_name='GROUCHO' or first_name='HARPO';
+
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
 
+update actor
+set first_name = 'GROUCHO'
+where first_name = 'HARPO';
+
+select * from actor
+where first_name='GROUCHO' or first_name='HARPO';
+
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
+describe address;
 
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
