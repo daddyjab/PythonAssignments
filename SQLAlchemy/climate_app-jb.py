79f5af1b98b857f1f0c18e5eab7aeb651212a314
Jeffery Brown <39058795+daddyjab@users.noreply.github.com>
2019-02-06 00:10:56 -0600

    Homework completed.

--- SQLAlchemy/climate_app-jb.py
+++ SQLAlchemy/climate_app-jb.py
@@ -1,3 +1,9 @@
+#################################################
+# Surf's Up! - Homework: 10-SQLAlchemy
+# Author: Jeffery Brown (daddyjab)
+# Date: 2/6/19
+#################################################
+
 import numpy as np
 
 import sqlalchemy
@@ -7,10 +13,12 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+import datetime as dt
+
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite?check_same_thread=False")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -37,15 +45,16 @@ app = Flask(__name__)
 def home():
     """List all available api routes."""
 
-    return (
-        f"Welcome to the Trip Planner API Endpoint:<br>"
-        f"Available Routes:<br>"
-        f"/api/v1.0/precipitation<br>"
-        f"/api/v1.0/stations<br>"
-        f"/api/v1.0/tobs<br>"
-        f"/api/v1.0/<start><br>"
-        f"/api/v1.0/<start>/<end><br>"
-    )
+    retval = """
+        Welcome to the Trip Planner API Endpoint:<br>
+        Available Routes:<br>
+        /api/v1.0/precipitation<br>
+        /api/v1.0/stations<br>
+        /api/v1.0/tobs<br>
+        /api/v1.0/start_date<br>
+        /api/v1.0/start_date/end_date<br>
+        """
+    return retval
 
 @app.route("/api/v1.0/precipitation")
 def precip():
@@ -55,7 +64,6 @@ def precip():
     results = session.query(Measurement)
 
     # Create a dictionary from the query results
-    # Create a dictionary from query results
     r_list = []
     for r in results:
         r_dict = {}
@@ -89,15 +97,87 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    pass
+    """List all the most recent 12 months of temperature measurements."""
 
+    # Determine the most recent date in the measurement data
+    results = session.query( func.max(Measurement.date) )
+    m_date_max = results[0][0]
+
+    # Determine the 12 month date window needed for the query
+    # Calc the End date of the most recent 12 month period
+    m_date_end = dt.datetime.strptime(m_date_max, "%Y-%m-%d")
+
+    # Calc the Start date of the 12 month period
+    m_date_start = m_date_end - dt.timedelta(days=365)
+
+    # Query 12 months of temperature data - columns: date and pcrp, ordered by: date
+    # Aggregate across multiple stations on the same date using average
+    results = session.query( Measurement.date, func.avg(Measurement.tobs) )\
+                        .filter(Measurement.date >= m_date_start, Measurement.date <= m_date_end)\
+                        .group_by(Measurement.date)\
+                        .order_by(Measurement.date).all()
+
+    # Populate the results in a dictionary
+    r_list = []
+    for r in results:
+        r_dict = {}
+        r_dict["date"] = r[0]
+        r_dict["avg_temperature"] = r[1]
+        r_list.append(r_dict)
+
+    # Generate the JSON return string
+    return jsonify(r_list)
+    
 @app.route("/api/v1.0/<start>")
 def temp_start(start):
-    pass
+    """List all temperature measurements greater than or equal to a specified start date."""
 
+    # Obtain the minimum, average, and maximum temperatures
+    # for all date greater than the specified start date
+    results = session.query(func.min(Measurement.tobs), \
+                            func.avg(Measurement.tobs), \
+                            func.max(Measurement.tobs)) \
+                            .filter(Measurement.date >= start)\
+                            .all()
+
+    # Unpack the query results into variables
+    temp_min, temp_avg, temp_max = results[0]
+
+    # Populate the results in a dictionary
+    r_dict = {
+        'max_temperature': temp_max,
+        'min_temperature': temp_min,
+        'avg_temperature': temp_avg
+    }
+
+    # Generate the JSON return string
+    return jsonify(r_dict)
+    
 @app.route("/api/v1.0/<start>/<end>")
 def temp_start_end(start, end):
-    pass
+    """List all temperature measurements in a specified range of dates."""
+
+    # Obtain the minimum, average, and maximum temperatures
+    # for all date greater than the specified start date
+    results = session.query(func.min(Measurement.tobs), \
+                            func.avg(Measurement.tobs), \
+                            func.max(Measurement.tobs)) \
+                            .filter(Measurement.date >= start)\
+                            .filter(Measurement.date <= end)\
+                            .all()
+
+    # Unpack the query results into variables
+    temp_min, temp_avg, temp_max = results[0]
+
+    # Populate the results in a dictionary
+    r_dict = {
+        'max_temperature': temp_max,
+        'min_temperature': temp_min,
+        'avg_temperature': temp_avg
+    }
+
+    # Generate the JSON return string
+    return jsonify(r_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
